767e57f78947f07e78373b44644a0a85900a8fbc
Cailuu <caiomotta07@gmail.com>
2025-04-08 01:29:13 +0000

    EM ANDAMENTO

--- GAME/Super_Trunfo.c
+++ GAME/Super_Trunfo.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 int main () {
-    int Populacao;
-    float Area;
-    float PIB;
-    int Turisticos;
+    char Estado = 'A';
+    float Area = 10.500;
+    float PIB = 50000000;
+    int Turisticos = 57;
+    char Nome[30] = "Belo Horizonte";
 
-    printf("População é: \n");
-    scanf("%d", &Populacao);
+    printf("A Area de %s é: %f\n", Nome, Area);
+    printf("O PIB DE %s é: %f\n", Nome, PIB);
+    
+}
 
-    return 0;
-
+    
 
 
 
