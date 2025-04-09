6995bba75470dcf6cd6805fdff6000f3fd77965f
Cailuu <caiomotta07@gmail.com>
2025-04-09 23:59:52 +0000

    Fim do nivel novato

--- GAME/Super_Trunfo.c
+++ GAME/Super_Trunfo.c
@@ -17,8 +17,29 @@ int main () {
     printf("População:%.3f\n", Populacao);
     printf("Area:%fkm²\n", Area);
     printf("PIB:%f Bilhoes de reais\n", PIB);
-    printf("Pontos turisticos %d\n", Turisticos);
+    printf("Pontos turisticos:%d\n", Turisticos);
+    printf("\n");
 
+    int Carta2 = 2;
+ 
+    char Codigo2[30] = "B01";
+    char Estado2 = 'B';
+    float Area2 = 13.800;
+    float PIB2 = 100500;
+    float Populacao2 = 480.000;
+    int Turisticos2 = 68;
+    char Nome2[30] = "Florianopolis";
+
+    printf("CARTA:%d\n", Carta2);
+    printf("ESTADO:%c\n", Estado2);
+    printf("Codigo:%s\n", Codigo2);
+    printf("Nome da cidade:%s\n", Nome2);
+    printf("População:%.3f\n", Populacao2);
+    printf("Area:%fkm²\n", Area2);
+    printf("PIB:%.4f Bilhoes de reais\n", PIB2);
+    printf("Pontos turisticos:%d\n", Turisticos2);
+
+    return 0;
 
 
 
