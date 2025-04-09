b20ce16f7fdddce6edcf2d4d277fdabf2a85ab97
Cailuu <caiomotta07@gmail.com>
2025-04-09 23:47:07 +0000

    REFEITO TODO O CODIGO

--- GAME/Super_Trunfo.c
+++ GAME/Super_Trunfo.c
@@ -1,15 +1,60 @@
 #include <stdio.h>
 int main () {
+   // Iniciando variaveis da carta 1
+    int Carta = 1;
+    char Codigo[30] = "A01";
     char Estado = 'A';
     float Area = 10.500;
-    float PIB = 50000000;
+    float PIB = 890.000000;
+    float Populacao = 380.000;
     int Turisticos = 57;
     char Nome[30] = "Belo Horizonte";
 
-    printf("A Area de %s é: %f\n", Nome, Area);
-    printf("O PIB DE %s é: %f\n", Nome, PIB);
-    
+    printf("CARTA:%d\n", Carta);
+    printf("ESTADO:%c\n", Estado);
+    printf("Codigo:%s\n", Codigo);
+    printf("Nome da cidade:%s\n", Nome);
+    printf("População:%.3f\n", Populacao);
+    printf("Area:%fkm²\n", Area);
+    printf("PIB:%f Bilhoes de reais\n", PIB);
+    printf("Pontos turisticos %d\n", Turisticos);
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+   
+    
 
     
 
